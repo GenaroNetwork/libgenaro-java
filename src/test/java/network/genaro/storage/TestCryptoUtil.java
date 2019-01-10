d8addb858d2d85c8d544907caf3428e44d488d9a
丁毅 <dingyi9982@qq.com>
2019-01-10 16:22:49 +0800

    update test.

--- src/test/java/network/genaro/storage/TestCryptoUtil.java
+++ src/test/java/network/genaro/storage/TestCryptoUtil.java
@@ -35,8 +35,6 @@ import static network.genaro.storage.CryptoUtil.sha256EscdaSign;
 
 @Test()
 public final class TestCryptoUtil {
-    private final static String V3JSON = "{ \"address\": \"fbad65391d2d2eafda9b27326d1e81d52a6a3dc8\", \"crypto\": { \"cipher\": \"aes-128-ctr\", \"ciphertext\": \"e968751f3d60827b6e62e3ff6c024ecc82f33a6c55428be33249c83edba444ca\", \"cipherparams\": { \"iv\": \"e80d9ec9ba6241a143c756ec78066ad9\" }, \"kdf\": \"scrypt\", \"kdfparams\": { \"dklen\": 32, \"n\": 262144, \"p\": 1, \"r\": 8, \"salt\": \"ea7cb2b004db67d3103b3790caced7a96b636762f280b243e794fb5bef8ef74b\" }, \"mac\": \"cdb3789e77be8f2a7ab4d205bf1b54e048ad3f5b080b96e07759de7442e050d2\" }, \"id\": \"e28f31b4-1f43-428b-9b12-ab586638d4b1\", \"version\": 3 }";
-
     public void test() throws Exception {
         byte[] byts = BasicUtil.string2Bytes("123kkk");
         byte[] ripemded = CryptoUtil.ripemd160Sha256(byts);
@@ -208,12 +206,4 @@ public final class TestCryptoUtil {
 //
 //        System.out.println(deMessage);
 //    }
-
-    public void testGetPrivateKey() throws Exception {
-        Genaro api = new Genaro(null, V3JSON, "lgygn_9982");
-
-        byte[] key = api.getPrivateKey();
-        String keyStr = Hex.toHexString(key);
-        Assert.assertEquals(keyStr, "61968cf440e7735e28bac3a431a81b49c3298d37910d54f62f1e5a32c518e556");
-    }
 }
